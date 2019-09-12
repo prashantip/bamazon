54e5d103e0766a086f065856d4e2805dfca07f7f
prashanti <iawwfte@gmail.com>
2019-09-11 21:32:03 -0400

    order the qty and insufficient qty

--- app.js
+++ app.js
@@ -1,47 +1,101 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var table = require("cli-table");
+require("console.table");
+//var Table = require("cli-table");
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-  // Your username
   user: "root",
-  // Your password
   password: "root",
   database: "bamazon_db"
 });
 
-
 connection.connect(function(err) {
   if (err) throw err;
-    console.error("connected as id " + connection.threadId);
-    display()
+  //  console.log("connected as id " + connection.threadId);
+  //  display()
 });
  // connection.connect();
  
-// console.log("hi");
-  function display(){
+  function start(){
     
     connection.query("SELECT * FROM products", function(err,res){
       
       if (err) throw err;
-     // console.log(res);
       console.log("Bamazon");
-      console.log("Product ID", "Product Name", "Department Name","Price", "Quantity");
-      // var table = new table({
-      //  head: ["Product ID", "Product Name", "Department Name","Price", "Quantity"],
-      //  colWidths: [10, 25, 25,10, 15]
-    //});
-    for(var i=0; i<res.length; i++){
-    //  table.push
-    console.log([res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].quantity]);
-  
-}  
-//console.log (table.toString());
+     console.table(res);
+      
+  //   console.log("Product ID", "Product Name", "Department Name","Price", "Quantity");
+//     var table = new Table({
+//     head: ["ID", "Product Name", "Department", "Price", "Stock"],
+//     colWidths: [5, 25, 25, 8, 5]
+// });
+     for(var i=0; i<res.length; i++){
+   
+  //  console.log([res[i].itemId, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quanity]);
+     //console.log ("-------------------------------");
+} 
 
- });    
-//display()
- } 
+ //display table
+ //  console.log(table.toString());
+
+    inquirer.prompt([{
+    name: "id",
+    type: "input",
+    message: "what is the ID of the product you would like to buy? ",
+    validate: function(value) {
+      if (isNaN(value) == false) {
+          return true
+      } else {
+          return false;
+      }
+       }
+
+       }, 
+    {
+      name: "quantity",
+      type: "input",
+      message: "how many would you like to buy? ",
+      validate: function(value) {
+        if (isNaN(value) == false) {
+            return true
+        } else {
+            return false;
+        }
+     } 
+   }
+      ]).then(function(answer){  
+      var quantity = answer.quantity;
+      var itemId = answer.id;    
+       connection.query("SELECT * FROM products WHERE ?",[{id: itemId}],function(err,selectedItem){
+         if(err)throw err;
+         if(selectedItem[0].stock_quantity - quantity>=0){
+
+          var orderTotal = quantity * selectedItem[0].price;
+
+          console.log("We have enough (" + selectedItem[0].product_name + ")!");
+          console.log("Quantity in stock: " + selectedItem[0].stock_quantity + " Order quantity: " + quantity);
+          console.log("You will be charged $" + orderTotal + ". Thank you!");
+
+          connection.query('UPDATE products SET stock_quantity=? WHERE id=?', [selectedItem[0].stock_quantity - quantity, itemId],
+                        function(err, inventory) {
+                            if (err) throw err;
+                           
+                          })
+               } else {
+                            console.log('Insufficient quantity.  Please adjust your order, we only have ' + selectedItem[0].stock_quantity + ' ' + selectedItem[0].product_name + 'in stock.');
+                          
+                       }
+                      });
+                    });
+
+          }); //query
+                  
+        } //start function
+        start();
+  
+   
+      
+  
+
--- bamazon.sql
+++ bamazon.sql
@@ -5,33 +5,33 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT (10) NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(45) NULL,
   department_name VARCHAR(45) NULL,
   price DECIMAL(10,2) NULL,
-  quantity INT NULL,
+  stock_quantity INT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (product_name, dempartment_name, price, quantity)
+INSERT INTO products (product_name, dempartment_name, price, stock_quantity)
 VALUES ("oatmeal", "groceries",3.50, 10);
-INSERT INTO products (product_name, dempartment_name, price, quantity)
+INSERT INTO products (product_name, dempartment_name, price, stock_quantity)
 VALUES ("whitebread", "groceries",3.10, 12);
-INSERT INTO products (Product_name, dempartment_name, price, quantity)
+INSERT INTO products (Product_name, dempartment_name, price, stock_quantity)
 VALUES ("strawberry","groceries", 2.25, 6);
-INSERT INTO products (Product_name, dempartment_name, price, quantity)
+INSERT INTO products (Product_name, dempartment_name, price, stock_quantity)
 VALUES ("toothbrush", "cosmetics",3.50, 30);
-INSERT INTO products (Product_name, dempartment_name, price, quantity)
-VALUES ("lotion", "cosmetics",6.50, 2);
-INSERT INTO products (Product_name, dempartment_name, price, quantity)
+INSERT INTO products (Product_name, dempartment_name, price, stock_quantity)
+VALUES ("lotion", "cosmetics",6.50, 1);
+INSERT INTO products (Product_name, dempartment_name, price, stock_quantity)
 VALUES ("painreliever", "pharmacy",3.50, 10);
-INSERT INTO products (Product_name, dempartment_name, price, quantity)
+INSERT INTO products (Product_name, dempartment_name, price, stock_quantity)
 VALUES ("sleepaid", "pharmacy",3.50, 3);
-INSERT INTO products (Product_name, dempartment_name, price, quantity)
+INSERT INTO products (Product_name, dempartment_name, price, stock_quantity)
 VALUES ("potatochips", "groceries",1.50, 8);
-INSERT INTO products (Product_name, dempartment_name, price, quantity)
+INSERT INTO products (Product_name, dempartment_name, price, stock_quantity)
 VALUES ("coke", "groceries",0.70, 10);
-INSERT INTO products (Product_name, dempartment_name, price, quantity)
+INSERT INTO products (Product_name, dempartment_name, price, stock_quantity)
 VALUES ("ballons", "seasonal",1.70, 3);
 
 
